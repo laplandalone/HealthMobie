c8f5cdfc3a077dc73571f2c1153949249db6ecc6
laplandalone <sunnyrhq@163.com>
2014-08-05 08:26:03 +0800

    8-5

---
+++ src/com/hbgz/pub/cache/PubData.java
@@ -0,0 +1,21 @@
+package com.hbgz.pub.cache;
+
+import java.util.List;
+
+import com.hbgz.pub.exception.QryException;
+import com.hbgz.pub.resolver.BeanFactoryHelper;
+
+public class PubData 
+{
+	public static List qryTeamList(String hospitalId) throws QryException
+	{
+		CacheManager cacheManager = (CacheManager) BeanFactoryHelper.getBean("cacheManager");
+		return cacheManager.qryTeamList(hospitalId);
+	}
+	
+	public static List qryDoctorList(String hospitalId) throws QryException
+	{
+		CacheManager cacheManager = (CacheManager) BeanFactoryHelper.getBean("cacheManager");
+		return cacheManager.qryDoctorList(hospitalId);
+	}
+}
