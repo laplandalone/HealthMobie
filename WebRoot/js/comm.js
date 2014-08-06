f886e964ac108c8ac0322b22cb59b555c745bc0a
laplandalone <sunnyrhq@163.com>
2014-08-06 14:02:08 +0800

    8-6

--- WebRoot/js/comm.js
+++ WebRoot/js/comm.js
@@ -1,39 +1,43 @@
-function lockScreen()
+function lockScreen() 
 {
-	
-	if(parent.document.getElementById("lockDiv")!=null&&parent.document.getElementById("lockDiv")!=undefined)
+	if (parent.document.getElementById("lockDiv") != null
+			&& parent.document.getElementById("lockDiv") != undefined) 
 	{
 		var lockDiv = parent.document.getElementById("lockDiv");
 		lockDiv.style.display = "block";
-	}
-	else if(parent.parent.document.getElementById("lockDiv")!=null&&parent.parent.document.getElementById("lockDiv")!=undefined)
+	} 
+	else if (parent.parent.document.getElementById("lockDiv") != null
+			&& parent.parent.document.getElementById("lockDiv") != undefined)
 	{
 		var lockDiv = parent.parent.document.getElementById("lockDiv");
 		lockDiv.style.display = "block";
 	}
-	else if(document.getElementById("lockDiv")!=null&&document.getElementById("lockDiv")!=undefined)
+	else if (document.getElementById("lockDiv") != null
+			&& document.getElementById("lockDiv") != undefined) 
 	{
 		var lockDiv = document.getElementById("lockDiv");
 		lockDiv.style.display = "block";
-	}		
+	}
 }
 
-
-function unlockScreen()
+function unlockScreen() 
 {
-	if(parent.document.getElementById("lockDiv")!=null&&parent.document.getElementById("lockDiv")!=undefined)
+	if (parent.document.getElementById("lockDiv") != null
+			&& parent.document.getElementById("lockDiv") != undefined) 
 	{
 		var lockDiv = parent.document.getElementById("lockDiv");
 		lockDiv.style.display = "none";
-	}
-	else if(parent.parent.document.getElementById("lockDiv")!=null&&parent.parent.document.getElementById("lockDiv")!=undefined)
+	} 
+	else if (parent.parent.document.getElementById("lockDiv") != null
+			&& parent.parent.document.getElementById("lockDiv") != undefined) 
 	{
 		var lockDiv = parent.parent.document.getElementById("lockDiv");
 		lockDiv.style.display = "none";
-	}
-	else if(document.getElementById("lockDiv")!=null&&document.getElementById("lockDiv")!=undefined)
+	} 
+	else if (document.getElementById("lockDiv") != null
+			&& document.getElementById("lockDiv") != undefined) 
 	{
 		var lockDiv = document.getElementById("lockDiv");
 		lockDiv.style.display = "none";
-	}	
+	}
 }
