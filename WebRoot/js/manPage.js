80fd254a52200b482a28849357ce7ceaeae49b30
laplandalone <sunnyrhq@163.com>
2014-08-06 17:06:10 +0800

    8-6

--- WebRoot/js/manPage.js
+++ WebRoot/js/manPage.js
@@ -1,4 +1,4 @@
-function queryFunc()
+function queryFunc() 
 {
 	var merOrderId = $("#merOrderId");
 	var platOrderFlow = $("#platOrderFlow");
@@ -6,79 +6,87 @@ function queryFunc()
 	merOrderId.val($.trim(merOrderId.val()));
 	platOrderFlow.val($.trim(platOrderFlow.val()));
 	platOrderId.val($.trim(platOrderId.val()));
-	if(merOrderId.val() == "" && platOrderFlow.val() == "" && platOrderId.val() == "")
+	if (merOrderId.val() == "" && platOrderFlow.val() == ""
+			&& platOrderId.val() == "") 
 	{
-		alertDialog("请输入要查询的信息");
+		$.dialog.alert("请输入要查询的信息", function(){return true;});
 	}
-	else
+	else 
 	{
 		$("#queryOrder").submit();
 	}
 }
 
-function refundClick()
+function refundClick() 
 {
 	var tranOrderId = $("#tranOrderId").val();
 	var bankCode = $("#bankCode").val();
 	var outTradeNo = $("#outTradeNo").val();
-	if(tranOrderId != "" && bankCode != "" && outTradeNo != "")
+	if (tranOrderId != "" && bankCode != "" && outTradeNo != "") 
 	{
 		var dig = null;
 		$.ajax({
 			url : "/transpond/refund.do",
 			type : "POST",
-			data : "tranOrderId="+tranOrderId+"&bankCode="+bankCode+"&outTradeNo="+outTradeNo,
-			beforeSend : function(){
+			data : "tranOrderId=" + tranOrderId + "&bankCode=" + bankCode + "&outTradeNo=" + outTradeNo,
+			beforeSend : function() 
+			{
 				dig = art.dialog({
-				    title : '正在退款请稍候....',
-				    lock : true
+					title : '正在退款请稍候....',
+					lock : true
 				});
 			},
-			success : function(result){
-				try{
+			success : function(result) 
+			{
+				try 
+				{
 					dig.close();
-				}catch(e)
-				{
-					
 				}
-				if(result == "success")
+				catch (e) 
+				{
+				}
+				if (result == "success") 
 				{
 					art.dialog({
-					    lock: true,
-					    content: "退款申请已提交!",
-					    icon: 'warning',
-					    ok: function () {
+						lock : true,
+						content : "退款申请已提交!",
+						icon : 'warning',
+						ok : function() 
+						{
 							window.location.href = "/platform/login.do";
-					        return true;
-					    },
-					});	
-				}
-				else
+							return true;
+						},
+					});
+				} 
+				else 
 				{
 					art.dialog({
-					    lock: true,
-					    content: "退款失败("+result+")，如有疑问请联系平台管理人员",
-					    icon: 'warning',
-					    ok: function () {
+						lock : true,
+						content : "退款失败(" + result + ")，如有疑问请联系平台管理人员",
+						icon : 'warning',
+						ok : function() 
+						{
 							window.location.href = "/platform/login.do";
-					        return true;
-					    },
-					});	
+							return true;
+						},
+					});
 				}
 			},
-			failure : function(result){
-				try{
-					dig.close();
-				}catch(e)
+			failure : function(result) 
+			{
+				try 
 				{
-					
+					dig.close();
+				} 
+				catch (e) 
+				{
 				}
-				alertDialog(result);
+				$.dialog.alert(result, function(){return true;});
 			}
-		})	
-	}
-	else
+		})
+	} 
+	else 
 	{
-		alertDialog("订单信息有误，请联系平台管理人员");
+		$.dialog.alert("订单信息有误，请联系平台管理人员", function(){return true;});
 	}
 }
